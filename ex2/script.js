79911dba1e3250ed496903c79df8072e7997cf48
VicenzoC <vicenzo_cavallaro@hotmail.com>
2023-10-31 11:38:37 -0300

    ex2

--- ex2/script.js
+++ ex2/script.js
@@ -1,5 +1,9 @@
 function carregar(ex) {
     footer(ex)
+    var data = document.getElementById('data')
+    var anoatual = new Date().getFullYear()
+    data.max = anoatual
+
 }
 
 function footer(ex) {
@@ -13,4 +17,34 @@ function footer(ex) {
     } else {
         previous.style.display = 'none'
     }
+}
+
+function verificar() {
+    var data = document.getElementById('data').value
+    var sexo = document.getElementById('sexo').value
+    var resultado = document.getElementById('resultado')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    var anoatual = new Date().getFullYear()
+    var gen = ''
+    if (sexo == 'M'){
+        gen = 'Homem'
+    } else {
+        gen = 'Mulher'
+    }
+    if (data > anoatual || data.length == 0) {
+        resultado.innerText = 'Ano inválido'
+    } else {
+        var idade = anoatual - data
+        resultado.innerText = `Detectamos ${gen} com ${idade} anos` 
+        if(idade < 12) {
+            img.src = `imagens/crianca${sexo}.jpg`
+        } else if (idade < 25) {
+            img.src = `imagens/jovem${sexo}.jpg`
+        } else if (idade < 65) {
+            img.src = `imagens/adulto${sexo}.jpg`
+        } else {
+            img.src = `imagens/idoso${sexo}.jpg`
+        }
+    }   resultado.appendChild(img)
 }
